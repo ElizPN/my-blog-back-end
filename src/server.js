94c74471685966d28eef210bbec8577cc14a1737
ElizPN <pankevych.liza@gmail.com>
2023-08-29 10:46:04 +0200

    add middlaware to receive requsts with json body

--- src/server.js
+++ src/server.js
@@ -2,7 +2,13 @@ import express from "express";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
+// middlaware. We tell server wnen it reciives request that has JSON body (JSON payload)
+// it is going to parce it and automatically make that available to us on request.body
+
+app.use(express.json());
+
+app.post("/hello", (req, res) => {
+  console.log(req.body);
   res.send("Helllllo");
 });
 
